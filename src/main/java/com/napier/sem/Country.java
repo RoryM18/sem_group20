4c20f8e8b9816495f9f9ef672ca8baadeec27b69
Matthew2904 <matthewgbsn88@gmail.com>
2022-02-28 13:07:13 +0000

    Query all countries in order of largest to smallest population

---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,88 @@
+package com.napier.sem;
+
+public class Country {
+
+
+    /**
+     *  Country code
+     */
+    public String code;
+
+    /**
+     *  Country Capital
+     */
+    public int capital;
+
+    /**
+     *  Country Code2
+     */
+    public String code2;
+
+    /**
+     *  Country Continent
+     */
+    public String continent;
+    /**
+     *  Country GNP
+     */
+
+    public double gnp;
+
+    /**
+     *  Country GNPOld
+     */
+
+    public double gnpOld;
+    /**
+     *  Country GovermentForm
+     */
+
+    public String govermentForm;
+
+    /**
+     *  Country HeadOfState
+     */
+
+    public String HeadOfState;
+    /**
+     *  Country IndepYear
+     */
+
+    public int indepYear;
+    /**
+     *  Country LifeExpectancy
+     */
+
+    public double lifeExpectancy;
+
+    /**
+     *  Country LocalName
+     */
+    public String localName;
+
+
+    /**
+     *  Country Name
+     */
+public String name;
+
+
+
+    /**
+     *  Country Population
+     */
+
+    public int population;
+
+    /**
+     *  Country Region
+     */
+
+    public String region;
+
+    /**
+     *  Country SurfaceArea
+     */
+    public double surfaceArea;
+
+}
