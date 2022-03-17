9f2cda44ba6264305eab0a2b681a1e8522da94e1
Rory Mackintosh <40492068@live.napier.ac.uk>
2022-03-17 14:54:56 +0000

    Testing output

--- src/main/java/com/napier/sem/City.java
+++ src/main/java/com/napier/sem/City.java
@@ -1,1 +1,14 @@
+package com.napier.sem;
 
+public class City {
+
+    public int ID;
+
+    public String name;
+
+    public String code;
+
+    public String district;
+
+    public int population;
+}
