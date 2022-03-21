f3c4b912bd02a693da2120d1b64eda2f0ff097df
Duncan-Hastie <98455487+Duncan-Hastie@users.noreply.github.com>
2022-03-21 11:41:41 +0000

    add the test data for the cities quires

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -23,7 +23,22 @@ public class AppIntegrationTest
     @Test
     void getCities()
     {
+        ArrayList<City> cities = app.getCities();
+        cities = app.getCitiesByAContinent();
 
+        int i = 0;
+        // checking the tenth line in the list of is correct (for the world)
+        for (City city: cities) {
+
+            i++;
+
+            if (i == 10)
+            {
+                assertEquals(city.name, "Tokyo");
+                assertEquals(city.population, "7980230");
+                break;
+            }
+        }
 
     }
 
@@ -34,15 +49,15 @@ public class AppIntegrationTest
         cities = app.getCitiesByAContinent();
 
         int i = 0;
-
+        // checking the seventh line in the list of is correct (for Oceania)
         for (City city: cities) {
 
             i++;
 
             if (i == 7)
             {
-                assertEquals(city.name, "");
-                assertEquals(city.population, "");
+                assertEquals(city.name, "Manukau");
+                assertEquals(city.population, "281800");
                 break;
             }
         }
@@ -51,19 +66,64 @@ public class AppIntegrationTest
     @Test
     void getCitiesByARegion()
     {
+        ArrayList<City> cities = app.getCities();
+        cities = app.getCitiesByRegion();
 
+        int i = 0;
+        // checking the fifth line in the list of is correct (for British Islands)
+        for (City city: cities) {
+
+            i++;
+
+            if (i == 5)
+            {
+                assertEquals(city.name, "Liverpool");
+                assertEquals(city.population, "461000");
+                break;
+            }
+        }
     }
 
     @Test
-    void getCitiesByACountrie()
+    void getCitiesByACountry()
     {
+        ArrayList<City> cities = app.getCities();
+        cities = app.getCitiesByACountry();
 
+        int i = 0;
+        // checking the third line in the list of is correct (for British Islands)
+        for (City city: cities) {
+
+            i++;
+
+            if (i == 3)
+            {
+                assertEquals(city.name, "Glasgow");
+                assertEquals(city.population, "619680");
+                break;
+            }
+        }
     }
 
     @Test
     void getCitiesByADisctrict()
     {
+        ArrayList<City> cities = app.getCities();
+        cities = app.getCitiesByADistrict();
 
+        int i = 0;
+        // checking the third line in the list of is correct (for Scotland)
+        for (City city: cities) {
+
+            i++;
+
+            if (i == 3)
+            {
+                assertEquals(city.name, "Aberdeen");
+                assertEquals(city.population, "213070");
+                break;
+            }
+        }
     }
 
 
