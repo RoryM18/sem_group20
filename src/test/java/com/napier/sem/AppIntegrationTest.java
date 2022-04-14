154994b4f616049e71c24cd6b4d97cd0fc70853c
Duncan-Hastie <40537030@live.napier.ac.uk>
2022-04-14 13:44:45 +0100

    add world pop test

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -1,1 +1,25 @@
+package com.napier.sem;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class AppIntegrationTest {
+    // Set up App
+    static App app;
+
+    @BeforeAll
+    static void init() {
+        app = new App();
+        app.connect("localhost:33060", 30000);
+
+    }
+
+    @Test
+    void getWorldPopulation()
+    {
+       String details = app.getWorldPopulation();
+       assertEquals(details, "world, 6078749450, 24%, 76%");
+    }
+}
