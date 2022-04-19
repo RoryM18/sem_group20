af30b4f381c0beb69f1546ef5d83f5e72997520b
Lier <40496253@live.napier.ac.uk>
2022-04-19 21:53:02 +0100

    Finishing Capitals Unit Test

--- src/test/java/com/napier/sem/AppTest.java
+++ src/test/java/com/napier/sem/AppTest.java
@@ -54,6 +54,43 @@ public class AppTest {
         country.code2 = "AW";
 
         countries.add(country);
+        app.displayCountries(countries, "tests displaying  country");
+    }
+
+    @Test
+    void displayCountries2Empty() {
+        ArrayList<Country> countries = new ArrayList<Country>();
+        app.displayCountries2(countries, "tests displaying empty list of countries");
+    }
+
+    @Test
+    void displayCountries2Null() {
+        ArrayList<Country> countries = new ArrayList<Country>();
+        countries.add(null);
+        app.displayCountries2(countries, "tests displaying null");
+    }
+
+    @Test
+    void displayCountries2() {
+        ArrayList<Country> countries = new ArrayList<Country>();
+        Country country = new Country();
+        country.code = "ABW";
+        country.name = "Aruba";
+        country.continent = "North America";
+        country.region = "Caribbean";
+        country.surfaceArea = 193.00;
+        country.indepYear = 0;
+        country.population = 103000;
+        country.lifeExpectancy = 78.4;
+        country.gnp = 828.00;
+        country.gnpOld = 793.00;
+        country.localName = "Aruba";
+        country.govermentForm = "Nonmetropolitan Territory of The Netherlands";
+        country.HeadOfState = "Beatrix";
+        country.capital = 129;
+        country.code2 = "AW";
+
+        countries.add(country);
         app.displayCountries2(countries, "tests displaying  country");
     }
 
@@ -110,5 +147,70 @@ public class AppTest {
         app.displayCountryLanguage(countrylanguages, "tests displaying a test city");
     }
 
+    void createNullCountry()
+    {
+        Country country = new Country();
+        country.code = null;
+        country.name = null;
+        country.continent = null;
+        country.region = null;
+        country.surfaceArea = Double.parseDouble(null);
+        country.indepYear = Integer.parseInt(null);
+        country.population = Integer.parseInt(null);
+        country.lifeExpectancy = Double.parseDouble(null);
+        country.gnp = Double.parseDouble(null);
+        country.gnpOld = Double.parseDouble(null);
+        country.localName = null;
+        country.govermentForm = null;
+        country.HeadOfState = null;
+        country.capital = Integer.parseInt(null);
+        country.code2 = null;
+
+
+    }
+
+    void createEmptyCountry()
+    {
+
+        Country country = new Country();
+        country.code = "";
+        country.name = "";
+        country.continent = "";
+        country.region = "";
+        country.surfaceArea = Double.parseDouble("");
+        country.indepYear = Integer.parseInt("");
+        country.population = Integer.parseInt("");
+        country.lifeExpectancy = Double.parseDouble("");
+        country.gnp = Double.parseDouble("");
+        country.gnpOld = Double.parseDouble("");
+        country.localName = "";
+        country.govermentForm = "";
+        country.HeadOfState = "";
+        country.capital = Integer.parseInt("");
+        country.code2 = "";
+
+
+    }
+
+    void createCapital()
+    {
+        Country country = new Country();
+        
+        country.code = "ABW";
+        country.name = "Aruba";
+        country.continent = "North America";
+        country.region = "Caribbean";
+        country.surfaceArea = 193.00;
+        country.indepYear = 0;
+        country.population = 103000;
+        country.lifeExpectancy = 78.4;
+        country.gnp = 828.00;
+        country.gnpOld = 793.00;
+        country.localName = "Aruba";
+        country.govermentForm = "Nonmetropolitan Territory of The Netherlands";
+        country.HeadOfState = "Beatrix";
+        country.capital = 129;
+        country.code2 = "AW";
+    }
 
 }
